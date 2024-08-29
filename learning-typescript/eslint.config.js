a8143339e6bc04f9886122abdd6d02f1f407b4dd
Adeleye Samuel Ayomide <116281277+Psalmwhuhell@users.noreply.github.com>
2024-08-29 14:43:12 +0000

    commit changes

--- learning-typescript/eslint.config.js
+++ learning-typescript/eslint.config.js
@@ -5,9 +5,9 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.eslintrc.cjs', 'postcss.config.cjs'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, 'plugin:storybook/recommended'],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
